0d2c964cb233ca6130cbd7f5b1e368d2f9d082f7
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-05-31 17:16:01 +0200

    Aggiunto faker per i commenti

---
+++ database/seeds/CommentsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Comment;
+use App\Post;
+use Faker\Generator as Faker;
+
+class CommentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $posts = Post::where('published', 1)->get();
+        foreach ($posts as $post) {
+            
+            for ($i=0; $i < rand(0, 3); $i++) { 
+                $newComment = new Comment();
+                $newComment->post_id = $post->id;
+                $newComment->name = $faker->name();
+                $newComment->content = $faker->text();
+                $newComment->save();
+            }
+        }
+    }
+}
